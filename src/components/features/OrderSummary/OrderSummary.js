35ea057d783b62c1ebebfc780037d6c4379c3fcc
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-07-24 19:08:23 +0200

    add trips prices and OrderFormContainer.js

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { Row, Col } from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
+import styles from './OrderSummary.scss';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+
+
+const OrderSummary = props => (
+  <h2 className={styles.component}>Total:<strong>{calculateTotal(formatPrice(props.tripCost), props.options)}</strong></h2>
+);
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
