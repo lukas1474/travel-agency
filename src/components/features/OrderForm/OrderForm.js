ed9704836db6475fc18d642a76e830a57dd020e8
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-07-30 12:21:13 +0200

    add order options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
+import pricing from '../../../data/pricing.json';
 
 const OrderForm = props => (
   <Row>
+    {pricing.map(option =>
+      <Col md={4} key={option.id}>
+        <OrderOption />
+      </Col>
+    )}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
     </Col>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+
+
+
+const OrderOption = () => (
+  <div className={styles.component}>
+    <h3 >Car Rental</h3>
+  </div>
+);
+
+// OrderOption.propTypes = {
+//   tripCost: PropTypes.string,
+//   options: PropTypes.object,
+// };
+
+export default OrderOption;
