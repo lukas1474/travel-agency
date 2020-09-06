f89e71c18b88b63e9f86c2eb9f034fc862efd32b
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-09-07 01:30:09 +0200

    add all unit tests for HappyHourAd component

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,12 +1,42 @@
 import React from 'react';
-// import styles from './HappyHourAd.scss';
+import PropTypes from 'prop-types';
+import styles from './HappyHourAd.scss';
 
 class HappyHourAd extends React.Component {
-  render() {
-    return (
-      <div />
-    );
+  constructor(){
+    super();
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
   }
+
+  getCountdownTime(){
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+
+    if(currentTime.getUTCHours() >= 12){
+      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  }
+
+  render(){
+    const {title, promoDescription} = this.props;
+    const getCountdownTime = this.getCountdownTime();
+
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>Happy Hour{title}</h3>
+        <p className={styles.promoDescription}>{getCountdownTime > 23 * 60 * 60 ? promoDescription : getCountdownTime}</p>
+      </div>
+    );}
 }
+
+HappyHourAd.propTypes = {
+  title: PropTypes.string,
+  promoDescription: PropTypes.string,
+};
+
 
 export default HappyHourAd;
